c1b5d47ea001aeff856501641d6466d67927ce01
florencialorenzo <lorenzo.mflorencia@gmail.com>
2022-06-14 16:32:12 -0300

    retries added

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 768,
   viewportWidth: 1024,
+  video: false,
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
