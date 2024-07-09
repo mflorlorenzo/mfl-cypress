01c6e9ea5ae16f9b9dc34cab2a4d9fb2fb7cae65
florencialorenzo <lorenzo.mflorencia@gmail.com>
2024-07-09 19:46:45 -0300

    new tests to check popup message and constants file created to store data

--- cypress/e2e/specs/home.cy.js
+++ cypress/e2e/specs/home.cy.js
@@ -1,6 +1,7 @@
 import { onHomepage } from "../../support/page_objects/homepage.js";
 import selectors from "../../fixtures/index.json";
 import urls from "../../fixtures/urls.json";
+import constants from "../../fixtures/constants.json";
 
 context("Cat Web", () => {
   describe("Homepage: ", { tags: "smoke" }, () => {
@@ -12,6 +13,13 @@ context("Cat Web", () => {
       cy.get(selectors.logoHomePageClass)
         .should("have.attr", "src")
         .should("include", urls.logoImg);
+    });
+
+    it("Popup should exists, be visible and have specific text", () => {
+      cy.get(selectors.homePopupClass).should(
+        "have.text",
+        constants.popupMessage
+      );
     });
   });
 });
